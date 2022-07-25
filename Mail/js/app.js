fad3857b96839b92c5719365b891653d475dc0df
Ste-cracco <s.cracco@yahoo.it>
2022-07-25 17:16:12 +0200

    Esercizio Mail completato

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -1,9 +1,17 @@
 let mailUtente = prompt("Inserisci e-mail")
 const mailConsentite = ["1@yahoo.it", "2@yahoo.it", "3@yahoo.it"]
-mailPresente = false
+let mailPresente = false
 
-if (mailUtente === mailConsentite) {
-        mailUtente = true;  
+for( let i = 0; i < mailConsentite.length; i++) {
+
+    if (mailUtente === mailConsentite[i]) {
+        mailPresente = true; 
+    }    
 }
 
-
+if(mailPresente) {
+    console.log("mail trovata")
+}
+else {
+    console.log ("mail non trovata")
+}
