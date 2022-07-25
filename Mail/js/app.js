62cb9a5ccde280a652ea1f821391afcdc0c62886
Ste-cracco <s.cracco@yahoo.it>
2022-07-25 16:26:20 +0200

    Esercizio Mail non risolto

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -1,1 +1,9 @@
+let mailUtente = prompt("Inserisci e-mail")
+const mailConsentite = ["1@yahoo.it", "2@yahoo.it", "3@yahoo.it"]
+mailPresente = false
+
+if (mailUtente === mailConsentite) {
+        mailUtente = true;  
+}
+
 
